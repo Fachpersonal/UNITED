45c43ff158bbf7a3f634a9dd1128c9cf0c79b3d5
Fachpersonal <falscherIdiot@gmail.com>
2022-10-06 14:38:46 +0200

    added getFileContent Methods

--- src/main/java/org/fachpersonal/helper/FileHelper.java
+++ src/main/java/org/fachpersonal/helper/FileHelper.java
@@ -1,6 +1,29 @@
 package org.fachpersonal.helper;
 
-public class FileHelper {
+import java.io.*;
 
+public class FileHelper {
+    public static String getFileContent(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            StringBuilder result = new StringBuilder();
+            while((line = br.readLine()) != null)
+            {
+                result.append(line).append("\n");
+            }
+            br.close();
+            return result.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String[] getFileContentLines(String path) {
+        String content = getFileContent(path);
+        if(content.contains("\n")) {
+            return content.split("\n");
+        }
+        return null;
+    }
 }
-qqq`
